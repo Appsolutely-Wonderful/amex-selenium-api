4fe1e46e1634ace5aa559c1dc93ff2f04819650a
Daniel Garcia-Briseno <daniel.garcia.briseno@appsolutelywonderful.com>
2021-01-09 00:30:49 -0700

    Successfully retrieved transactions

--- amex_selenium.py
+++ amex_selenium.py
@@ -10,28 +10,150 @@ Credits: This is possible thanks to the developers of selenium and selenium-pyth
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from time import time, sleep
+from transaction import Transaction
 
-
-class AmexSelenium:
+class AmexAPI:
+    """
+    Provides an interface for the amex website
+    """
     LOGIN_URL = "https://www.americanexpress.com/en-us/account/login"
+    TRANSACTIONS_URL = "https://global.americanexpress.com/activity/recent"
     USER_ID_FIELD_ID = "eliloUserID"
+    PASSWORD_FIELD_ID = "eliloPassword"
+    LOGIN_BTN_ID = "loginSubmit"
     EXPECTED_TITLE_CONTENTS = "American Express"
+    timeout = 30 # seconds
     def __init__(self):
         """
         Initializes the selenium driver
         """
-        self.driver = webdriver.Firefox()
-        self.driver.get(self.LOGIN_URL)
-        elem = self.driver.find_element_by_name(self.USER_ID_FIELD_ID)
-        elem.clear()
-        elem.send_keys("pycon")
-        elem.send_keys(Keys.RETURN)
-        assert "No results found." not in self.driver.page_source
+        opts = Options()
+        # opts.set_headless()
+        self.driver = webdriver.Firefox(options=opts)
+
+    def close(self):
+        """
+        Close selenium driver
+        """
         self.driver.close()
+
+    def wait_for_title_update(self, title):
+        """
+        Wait a predetermined timeout for the page title to update
+        """
+        start_time = time()
+        while title.lower() not in self.driver.title.lower():
+            now = time()
+            if now > (start_time + self.timeout):
+                print("Timeout waiting for page title to change.")
+                print(f"Expected '''{title}'''s")
+                print(f"Current title is {self.driver.title}")
+            sleep(1)
+
+        assert title.lower() in self.driver.title.lower()
+
+    def wait_for_view_transactions_button(self):
+        try:
+            element_present = EC.presence_of_element_located((By.LINK_TEXT, 'View Transactions'))
+            WebDriverWait(self.driver, self.timeout).until(element_present)
+        except TimeoutException:
+            print("Timed out waiting for page to load")
+        finally:
+            print("Page loaded")
+
+    def _wait_for_load(self, by, val):
+        """
+        Waits for the given xpath element to be loaded
+        """
+        try:
+            element_present = EC.presence_of_element_located((by, val))
+            WebDriverWait(self.driver, self.timeout).until(element_present)
+        except TimeoutException:
+            print("Timed out waiting for page to load")
+            raise TimeoutError
+
+    # def __del__(self):
+    #     """
+    #     Close selenium driver when object is destroyed
+    #     """
+    #     self.driver.close()
 
     def login(self, username, password):
         """
-        Login to the american express website
+        Login to the american express website by filling in the login
+        form with the provided username and password
         """
-        self.driver.get(LOGIN_URL)
-        assert EXPECTED_TITLE_CONTENTS in driver.title
+        self.driver.get(self.LOGIN_URL)
+        assert self.EXPECTED_TITLE_CONTENTS in self.driver.title
+        user_field = self.driver.find_element_by_id(self.USER_ID_FIELD_ID)
+        user_field.send_keys(username)
+        password_field = self.driver.find_element_by_id(self.PASSWORD_FIELD_ID)
+        password_field.send_keys(password)
+        submit_btn = self.driver.find_element_by_id(self.LOGIN_BTN_ID)
+        submit_btn.click()
+        self.wait_for_title_update("dashboard")
+        self.wait_for_view_transactions_button()
+        print("Successfully logged in")
+
+    def _navigate_to_transactions(self):
+        """
+        Navigate to Amex transactions page
+        """
+        self.driver.get(self.TRANSACTIONS_URL)
+        self._wait_for_load(By.ID, 'select-all-transactions')
+
+    def _click_skip_tour_if_popup(self):
+        try:
+            el = self.driver.find_element_by_id('skip-link-0')
+            el.click()
+        except NoSuchElementException:
+            pass
+
+    def _retrieve_recent_transactions(self):
+        """
+        Retrieves transactions from the current page.
+        This is expected to run after _navigate_to_transactions
+
+        Returns: List of Transaction objects
+        """
+        assert "Account Activity" in self.driver.title, "Not on account page"
+        self._click_skip_tour_if_popup()
+        elements = self.driver.find_elements_by_xpath("//div[contains(@id, 'transaction_')]")
+        if len(elements) > 0:
+            while elements[0].text.strip() == '':
+                sleep(1)
+                elements = self.driver.find_elements_by_xpath("//div[contains(@id, 'transaction_')]")
+        transactions = []
+        for el in elements:
+            pieces = el.text.split('\n')
+            # idx 0 - date
+            # idx 1 - Pending or Merchant
+            # idx 2 - If pending: merchant, else amount
+            # idx 3 - If pending: amount, else out of bounds
+            pending = pieces[1] == 'Pending'
+            date = pieces[0]
+            if not pending:
+                merchant = pieces[1]
+                amount = pieces[2]
+            else:
+                merchant = pieces[2]
+                amount = pieces[3]
+            transactions.append(Transaction(date=date, merchant=merchant, amount=amount, pending=pending))
+        return transactions
+
+
+    def get_transactions(self, since_date):
+        """
+        Retrieve all cleared transactions starting at since_date
+
+        Pre-requisites: Must first execute login
+        """
+        self._navigate_to_transactions()
+        transactions = self._retrieve_transactions()
